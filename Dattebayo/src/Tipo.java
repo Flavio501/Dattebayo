1ed3d35e1a13cb33f43233471f7b34e223671689
flavio501 <flavio501@flavio501>
2018-10-02 17:23:23 -0700

    Floats y Strings corregidos

--- Dattebayo/src/Tipo.java
+++ Dattebayo/src/Tipo.java
@@ -1,15 +1,16 @@
 
 public enum Tipo {
     ENTEROS("^\\d+$"),
-    STRINGS("[A-Za-z*]+$"),
-    OP_ARIT("[*|/|+|-|^]$"),
+    STRINGS("\"(.*?)\""),
+    OP_ARIT("[* | / | + | - | ^ ]$"),
     OP_LOG("neji|sasori|deidara"),
     ASIGNACION("="),
     PALABRAS_RESERVADAS("(\n|abs|brk|cs|char|cl|do|else|extends|for|if|implements|import|infa|swt|this|try|wl|yes|no|bl|bt|st"
             + "str|int|ft|dou)$"),
     IDENTIFICADORES("^_[A-Za-z|0-9]+$"),
     AGRUPADORES("[{|}|[|]|(|)]$"),
-    TERMINADORES(";");
+    TERMINADORES("(;)"),
+    FLOATS("[0-9]*[.][0.9]*");
 
     public final String patron;
 
