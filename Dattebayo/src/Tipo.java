33db03d3897d6ae102ca381dc60bb4a43ae44996
Hermes <hermes.castro@cetys.edu.mx>
2018-10-09 18:46:40 -0700

    aPERTURA Y CIERRE

--- Dattebayo/src/Tipo.java
+++ Dattebayo/src/Tipo.java
@@ -1,13 +1,14 @@
 
 public enum Tipo {
+	AGRUPADORES_APERTURA("[\\{|\\[|\\(]"),
+    AGRUPADORES_CIERRE("[\\}|\\]|\\)]"),
     ENTEROS("^\\d+$"),
     STRINGS("\"(.*?)\""),
     OP_ARIT("[* | / | + | - | ^ ]$"),
-    OP_LOG("neji|sasori|deidara|gui|lee|gaara|choji|hidan"),
+    OP_LOG("\\b(neji|sasori|deidara|gui|lee|gaara|choji|hidan)\\b"),
     ASIGNACION("="),
     PALABRAS_RESERVADAS("(\n|Hokage|byakugan|sharingan|rinnegan|kamui|tsukuyomi|kurama|gyuki|genin|jonin)$"),
     IDENTIFICADORES("^_[A-Za-z|0-9]+$"),
-    AGRUPADORES("[ { | } | \\[ | \\] | \\( | \\) ]$"),
     TERMINADORES("\\;"),
     TIPOS("chidori|rasengan|susanoo|chakra|shuriken"),
     FLOATS("[0-9]*[.][0.9]*");
